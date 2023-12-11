580e6419f9e6c891b1a643f8c711dbb9cf26f84a
okdi <newmailri@gmail.com>
2023-12-12 00:29:53 +0400

    fix description columns

--- database/migrations/2023_12_10_204324_create_compositions_table.php
+++ database/migrations/2023_12_10_204324_create_compositions_table.php
@@ -10,7 +10,7 @@ return new class extends Migration {
         Schema::create('compositions', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('description');
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
